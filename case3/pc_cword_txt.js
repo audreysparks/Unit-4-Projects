eb7cf0defd8183b96bb6ad4e9632b30bd1017830
audreysparks <53583299+audreysparks@users.noreply.github.com>
2020-04-02 00:14:30 -0700

    Update pc_cword_txt.js

--- case3/pc_cword_txt.js
+++ case3/pc_cword_txt.js
@@ -48,11 +48,53 @@
 
 
 */
+//step 4 sets each variable to be called 
+var allLetters;
+var currentLetter;
+var wordLetters;
+var acrossLetter;
+var acrossClue;
+var downClue;
+var typeDirection = "right";
 
+//step 5 allows for the code to show on the screen
+window.onload = init();
+ 
+//step 6 allows for the clues to pop up from either going across or down 
+function init() {
+allLetters = document.querySelectorAll("table#crossword span");
+currentLetter = allLetters[0];
+var acrossID = currentLetter.dataset.clueA;
+var downID = currentLetter.dataset.clueD;
+acrossClue = document.getElementById(currentLetter.dataset.clueA);
+downClue = document.getElementById(currentLetter.dataset.clueD);
+}
 
-
-
-
+//step7 sets the color for when you either go across or down on the game
+function formatPuzzle(puzzleLetter) {
+   currentLetter = puzzleLetter;
+   
+   for(var i = 0; i < allLetters.length; i++){
+      allLetters[i].style.backgroundColor = "";
+   }
+   acrossClue.style.color = "";
+   downClue.style.color = "";
+   
+   acrossClue = document.getElementById(currentLetter.dataset.clueA);
+   acrossClue.style.color = blue;
+   wordLetters.style.backgroundColor = "rgb(231, 231, 255)";
+   
+   downClue = document.getElementById(currentLetter.dataset.clueA);
+   downClue.style.color = red;
+   wordLetters.style.backgroundColor = "rgb(255, 231, 231)";
+   
+   if(typeDirection *** "right"){
+      currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+   }else if{
+      currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+      )
+   
+   
    
 
 
